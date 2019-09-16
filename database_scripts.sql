358c816608b2a992d98d84a6ba041cab691530b8
nadia-said <46871994+nadia-said@users.noreply.github.com>
2019-09-16 12:26:37 -0400

    Add files via upload
    these are the database scripts needed for uploading. will continuously update as new tables are added in.

---
+++ database_scripts.sql
@@ -0,0 +1,36 @@
+CREATE TABLE [dbo].[BulkUpload](
+[UploadID] INT IDENTITY(1,1) NOT NULL, 
+[AdminAccessnet] VARCHAR(MAX) NOT NULL, 
+[DateUploaded] DATE NOT NULL, 
+PRIMARY KEY CLUSTERED ([UploadID] ASC) 
+); 
+CREATE TABLE [dbo].[Location](
+[LocationID] INT IDENTITY (1,1) NOT NULL, 
+[Watershed ID] INT NOT NULL, 
+[Longitude] FLOAT(53) NOT NULL, 
+[Latitude] FLOAT (53) NOT NULL, 
+[SensorName] VARCHAR(MAX) NOT NULL, 
+[SerialNumber] VARCHAR(MAX) NULL. 
+[ProfileImage] VARCHAR(MAX) NULL, 
+[LastUpdated] DATE NOT NULL, 
+PRIMARY KEY CLUSTERED ([LocationID] ASC) ,
+CONSTRAINT  [FK_Location_ToTable] FOREIGN KEY ([WatershedID]) REFERENCES [dbo].[Watershed] ([WatershedID]) 
+); 
+CREATE TABLE [dbo].[Temperature]( 
+[TempID] INT IDENTITY(1,1) NOT NULL, 
+[LocationID] INT NOT NULL, 
+[UploadID] INT NOT NULL, 
+[DateRecorded] DATE NOT NULL, 
+[TimeRecorded] TIME(7)  NOT NULL, 
+[TempC] FLOAT(53) NOT NULL, 
+[TempF] FLOAT(53) NOT NULL, 
+PRIMARY KEY CLUSTERED ([TempID] ASC) , 
+CONSTRAINT [FK_Temperature_ToTable1] FOREIGN KEY ([UploadID]) REFERENCES [dbo].[BulkUpload] ([UploadID]). 
+CONSTRAINT [FK_Temperature_ToTable] FOREIGN KEY ([LocationID]) REFERENCES [dbo].[Location] ([LocationID]) 
+); 
+CREATE TABLE [dbo].[Watershed] (
+[WatershedID] INT IDENTITY (1,1) NOT NULL, 
+[WatershedName] VARCHAR (MAX) NOT NULL, 
+[LastUpdated] DATE NOT NULL. 
+PRIMARY KEY CLUSTERED ([WatershedID] ASC)
+); 
