1a798ae870bba1e579fb2f4faf2207b21686364d
Nadia S Said <tug34018@temple.edu>
2019-11-13 23:31:23 -0500

    edit and create for admins fixed

--- CitizenScienceClasses/Admin.cs
+++ CitizenScienceClasses/Admin.cs
@@ -10,18 +10,41 @@ namespace CitizenScienceClasses
     {
         private int adminID;
         private string tuid;
+        private string fname;
+        private string lname;
+        private string email; 
         private bool active;
+
         public Admin() { }
-        public Admin(int id, string tu, bool act)
+        public Admin(int id, string tu,string fn, string ln, string em, bool act)
         {
             adminID = id;
             tuid = tu;
             active = act;
+            fname = fn;
+            lname = ln;
+            Email = em; 
         }
         public int AdminID
         {
             get { return adminID; }
             set { adminID = value; }
+        }
+
+        public string FName
+        {
+            get { return fname; }
+            set { fname = value;  }
+        }
+        public string LName
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
         }
         public string TUID
         {
